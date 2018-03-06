a734fee0cd0481521955c61dd4fe3d46cc27f007
sergey.tereshkin <serj.tereshkin@gmail.com>
2018-03-06 10:02:35 +0100

    new video launcher

--- ReTube/Controller/VideoLauncher.swift
+++ ReTube/Controller/VideoLauncher.swift
@@ -7,134 +7,59 @@
 //
 
 import UIKit
-import AVFoundation
-
-class VideoPlayerView: UIView {
-    
-    let activityIndicatorView: UIActivityIndicatorView = {
-        let indicator = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
-        indicator.translatesAutoresizingMaskIntoConstraints = false
-        indicator.startAnimating()
-        return indicator
-    }()
-    
-    let controlsContainerView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .black
-        return view
-    }()
-    
-    lazy var pausePlayButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "ic_pause"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.tintColor = .white
-        button.isHidden = true
-        button.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
-        return button
-    }()
-    
-    var isPlaying = false
-    
-    @objc func handlePlayPause() {
-        
-        if isPlaying {
-            player?.pause()
-            pausePlayButton.setImage(#imageLiteral(resourceName: "ic_play_arrow"), for: .normal)
-        } else {
-            player?.play()
-            pausePlayButton.setImage(#imageLiteral(resourceName: "ic_pause"), for: .normal)
-        }
-        
-        isPlaying = !isPlaying
-    }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-//        controlsContainerView.frame = CGRect(x: 0, y: 0, width: frame.width, height: frame.height)
-        addSubview(controlsContainerView)
-        controlsContainerView.addSubview(activityIndicatorView)
-        controlsContainerView.addSubview(pausePlayButton)
-        
-        setupPlayerView()
-        
-        NSLayoutConstraint.activate([
-            controlsContainerView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            controlsContainerView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            activityIndicatorView.centerXAnchor.constraint(equalTo: controlsContainerView.centerXAnchor),
-            activityIndicatorView.centerYAnchor.constraint(equalTo: controlsContainerView.centerYAnchor),
-            pausePlayButton.centerXAnchor.constraint(equalTo: controlsContainerView.centerXAnchor),
-            pausePlayButton.centerYAnchor.constraint(equalTo: controlsContainerView.centerYAnchor),
-            pausePlayButton.widthAnchor.constraint(equalToConstant: 70),
-            pausePlayButton.heightAnchor.constraint(equalToConstant: 70)
-            ])
-    }
-    
-    var player: AVPlayer?
-    
-    func setupPlayerView() {
-        
-        let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
-        
-        backgroundColor = .black
-        if let url = NSURL(string: urlString) {
-            player = AVPlayer(url: url as URL)
-            let playerLayer = AVPlayerLayer(player: player)
-            self.layer.addSublayer(playerLayer)
-            playerLayer.frame = controlsContainerView.frame
-            player?.play()
-            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
-        }
-    }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "currentItem.loadedTimeRanges" {
-            activityIndicatorView.stopAnimating()
-            controlsContainerView.backgroundColor = .clear
-            pausePlayButton.isHidden = false
-            isPlaying = true
-        }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
 
 class VideoLauncher: NSObject {
     
+    // constant and static members
     static let sharedInstance = VideoLauncher()
     
     let kMinimizedPlayerHeight: CGFloat = 75
-    let kMinimizedPlayerMargin: CGFloat = 20
+    let kMinimizedPlayerMargin: CGFloat = 16
     
+    // variable memebers
+    var lastTouch: CGPoint?
+    
+    // views
     let mainView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        view.backgroundColor = .clear
         return view
     }()
     
-    let minimizedVideoPlayer: UIView = {
-        let mplayer = UIView()
+    let minimizedPlayerView: MinimizedPlayerView = {
+        let mplayer = MinimizedPlayerView()
         mplayer.backgroundColor = .green
-        //        mplayer.isHidden = true
         mplayer.isUserInteractionEnabled = true
+        mplayer.alpha = 0
         return mplayer
+    }()
+    
+    let playerView: MainPlayerView = {
+        let player = MainPlayerView()
+        player.backgroundColor = .green
+        player.isUserInteractionEnabled = true
+        return player
+    }()
+    
+    let bottomView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
     }()
     
     let keyWindow: UIWindow = {
         return UIApplication.shared.keyWindow ?? UIWindow()
     }()
     
-    let isVisible: Bool = false
+    var playerOriginalFrame: CGRect?
     
+    // initialization
     override init() {
         super.init()
         
         if let keyWindow = UIApplication.shared.keyWindow {
             
+            let statusBarHeight = UIApplication.shared.statusBarFrame.height
             let minimizedPlayerOffset = kMinimizedPlayerHeight + kMinimizedPlayerMargin
             
             mainView.frame = CGRect(x: 0,
@@ -143,82 +68,159 @@ class VideoLauncher: NSObject {
                                     height: keyWindow.frame.height + minimizedPlayerOffset)
             
             let playerHeight = keyWindow.frame.width * 9 / 16
-            let videoPlayerView = VideoPlayerView(frame: CGRect(x: 0,
-                                                                y: minimizedPlayerOffset,
-                                                                width: keyWindow.frame.width,
-                                                                height: playerHeight))
+            playerView.frame = CGRect(x: 0,
+                                      y: minimizedPlayerOffset + statusBarHeight,
+                                      width: keyWindow.frame.width,
+                                      height: playerHeight)
             
-            minimizedVideoPlayer.frame = CGRect(x: kMinimizedPlayerMargin,
+            playerOriginalFrame = playerView.frame
+            
+            minimizedPlayerView.frame = CGRect(x: kMinimizedPlayerMargin,
                                                 y: 0,
                                                 width: mainView.frame.width - (kMinimizedPlayerMargin * 2),
                                                 height: kMinimizedPlayerHeight)
             
-            mainView.addSubview(videoPlayerView)
-            mainView.addSubview(minimizedVideoPlayer)
+            bottomView.frame = CGRect(x: 0, y: playerHeight + minimizedPlayerOffset + statusBarHeight, width: keyWindow.frame.width, height: keyWindow.frame.height - playerHeight - statusBarHeight)
+            
+            let fakePlayerView = UIView()
+            fakePlayerView.frame = playerView.frame
+            fakePlayerView.backgroundColor = .white
+            
+            mainView.addSubview(bottomView)
+            mainView.addSubview(minimizedPlayerView)
+            mainView.addSubview(fakePlayerView)
+            mainView.addSubview(playerView)
             keyWindow.addSubview(mainView)
             
             let mainViewGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
             let minimizedPlayerGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
             
-            videoPlayerView.addGestureRecognizer(mainViewGestureRecognizer)
-            minimizedVideoPlayer.addGestureRecognizer(minimizedPlayerGestureRecognizer)
+            playerView.addGestureRecognizer(mainViewGestureRecognizer)
+            minimizedPlayerView.addGestureRecognizer(minimizedPlayerGestureRecognizer)
+            minimizedPlayerView.addGestureRecognizer(tapGesture)
         }
     }
     
+    // show, hide minimize helper functions
     func showVideoPlayer() {
-        
         let minimizedPlayerOffset = kMinimizedPlayerHeight + kMinimizedPlayerMargin
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(orientationDidChange), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             self.mainView.frame = CGRect(x: 0,
                                          y: -minimizedPlayerOffset,
                                          width: self.mainView.frame.width,
                                          height: self.mainView.frame.height)
+            self.minimizedPlayerView.alpha = 0
         }, completion: { (_) in
-            //                UIApplication.shared.isStatusBarHidden = true
+            // maybe I will do something later here
         })
     }
     
     func minimizeVideoPlayer() {
         let minimizedPlayerOffset = kMinimizedPlayerHeight + kMinimizedPlayerMargin
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
         
         UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
             self.mainView.frame = CGRect(x: 0,
                                          y: self.keyWindow.frame.height - minimizedPlayerOffset,
                                          width: self.mainView.frame.width,
                                          height: self.mainView.frame.height)
+            self.minimizedPlayerView.alpha = 1
         }, completion: { (_) in
-            
+            // maybe I will do something later here
         })
     }
     
-    func hideVideoPlayer() {
-        // to do
+    func closeVideoPlayer() {
+        UIView.animate(withDuration: 0.7, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+            self.mainView.frame = CGRect(x: 0,
+                                         y: self.keyWindow.frame.height,
+                                         width: self.mainView.frame.width,
+                                         height: self.mainView.frame.height)
+            self.minimizedPlayerView.alpha = 0
+        }, completion: { (_) in
+            // maybe I will do something later here
+        })
     }
     
-    var globalTouchLocatino: CGPoint?
-    
+    // MARK - Gesture recognizerz
     @objc func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
-        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+        
+        if UIDevice.current.orientation != .portrait {
+            return
+        }
+        
+        switch gestureRecognizer.state {
+        case .began, .changed:
+            // move player view
+            lastTouch = gestureRecognizer.location(in: keyWindow)
             
-            globalTouchLocatino = gestureRecognizer.location(in: keyWindow)
+            if  let view = gestureRecognizer.view {
+                let translation = gestureRecognizer.translation(in: mainView)
+                let newCenterPoint = CGPoint(x: mainView.frame.size.width / 2, y: mainView.center.y + translation.y)
+                
+                if let superview = view.superview {
+                    superview.center = newCenterPoint
+                    gestureRecognizer.setTranslation(CGPoint.zero, in: mainView.superview)
+                }
+            }
             
-            let translation = gestureRecognizer.translation(in: mainView)
-            
-            let point = CGPoint(x: mainView.frame.size.width / 2, y: mainView.center.y + translation.y)
-            
-            gestureRecognizer.view!.superview!.center = point
-            gestureRecognizer.setTranslation(CGPoint.zero, in: mainView.superview)
-            
-        } else if gestureRecognizer.state == .ended {
-            
-            if let touch = globalTouchLocatino {
-                if touch.y > keyWindow.center.y {
+            break;
+        case .ended:
+            // minimize or show full open player
+            if let lastTouch = lastTouch {
+                if lastTouch.y > keyWindow.center.y {
                     minimizeVideoPlayer()
-                } else if !UIApplication.shared.isStatusBarHidden {
+                } else {
                     showVideoPlayer()
                 }
             }
+            break;
+            
+        default:
+            // there's nothing I can do here
+            print("Print statement to make compiler happy")
+        }
+    }
+    
+    @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
+        showVideoPlayer()
+    }
+    
+    // MARK - Device orientation callback
+    @objc func orientationDidChange() {
+        
+        switch UIDevice.current.orientation {
+        case .landscapeLeft:
+            let y = kMinimizedPlayerHeight + kMinimizedPlayerMargin
+            UIView.animate(withDuration: 0.3, animations: {
+                UIApplication.shared.isStatusBarHidden = true
+                self.playerView.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 2)
+                self.playerView.frame = CGRect(x: 0, y: y, width: self.keyWindow.frame.width, height: self.keyWindow.frame.height)
+            })
+            
+            break;
+        case .landscapeRight:
+            let y = kMinimizedPlayerHeight + kMinimizedPlayerMargin
+            UIView.animate(withDuration: 0.3, animations: {
+                UIApplication.shared.isStatusBarHidden = true
+                self.playerView.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+                self.playerView.frame = CGRect(x: 0, y: y, width: self.keyWindow.frame.width, height: self.keyWindow.frame.height)
+            })
+            break;
+        case .portrait:
+            UIView.animate(withDuration: 0.3, animations: {
+                UIApplication.shared.isStatusBarHidden = false
+                self.playerView.transform = CGAffineTransform(rotationAngle: 0)
+                self.playerView.frame = self.playerOriginalFrame ?? CGRect.zero
+            })
+            
+            break;
+        default:
+            print("Unknown orientation")
+            break;
         }
     }
 }
